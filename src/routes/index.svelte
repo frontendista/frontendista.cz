5f75b3374af2b1c9a9691ffa23ebef73c70821f2
Pavel Sušický <susicky.pavel@outlook.cz>
2022-04-26 22:23:41 +0200

    chore(import): add absolute imports shortcuts

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,10 @@
+<script lang="ts">
+	import TemporaryCommon from '$common/temporary-common.svelte';
+	import TemporaryModule from '$modules/temporary-module.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<TemporaryCommon />
+<TemporaryModule />
--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,25 @@
+import { resolve } from 'path';
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		vite: {
+			resolve: {
+				alias: {
+					$common: resolve('./src/lib/components/__common__'),
+					$components: resolve('./src/lib/components'),
+					$modules: resolve('./src/lib/modules'),
+					$stores: resolve('./src/lib/stores'),
+					$stylesheets: resolve('./src/lib/stylesheets'),
+					$utils: resolve('./src/lib/utils')
+				}
+			}
+		}
 	}
 };
 
