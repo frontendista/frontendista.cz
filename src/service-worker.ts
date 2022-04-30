d75008b3d681f7c607eff9cb69e6653168e9cbdc
Pavel Sušický <susicky.pavel@outlook.cz>
2022-04-30 19:58:12 +0200

    feat(sw): add service-worker.ts

---
+++ src/service-worker.ts
@@ -0,0 +1,16 @@
+// TODO: Re-check before releasing 1.0.0
+
+import { version } from "$service-worker";
+
+self.addEventListener("install", (e) => {
+	console.log("Installed: ", e);
+	console.log("Version: ", version);
+});
+
+self.addEventListener("activate", (e) => {
+	console.log("Activated: ", e);
+});
+
+self.addEventListener("event", (e) => {
+	console.log("Event: ", e);
+});
