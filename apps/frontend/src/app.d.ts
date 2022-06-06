e19a762a73e8230b9af3bcd67d35d3dbd3b26592
Pavel Sušický <susicky.pavel@outlook.cz>
2022-06-06 21:39:32 +0200

    chore(env): add vars to TypeScript global namespace

--- apps/frontend/src/app.d.ts
+++ apps/frontend/src/app.d.ts
@@ -15,3 +15,11 @@ declare global {
 		type Assertion<T = any> = TestingLibraryMatchers<T, void>;
 	}
 }
+
+declare namespace NodeJS {
+	interface ProcessEnv {
+		readonly SANITY_PROJECT_ID: string;
+		readonly SANITY_DATASET_ID: string;
+		readonly SANITY_AUTH_TOKEN: string;
+	}
+}
