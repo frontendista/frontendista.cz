9ba9b14129ba4a5befb9689831892e4e74ca4e1f
Pavel Sušický <susicky.pavel@outlook.cz>
2022-06-08 18:48:11 +0200

    feat(post): add PortableText

--- apps/frontend/src/routes/blog/[slug].svelte
+++ apps/frontend/src/routes/blog/[slug].svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import type { BlogPostContent } from "$utils/queries/post-content";
+	import { PortableText } from "@portabletext/svelte";
 
 	export let post: BlogPostContent;
 </script>
 
 <h1>{post.title}</h1>
 <time datetime={post.publishedAt}>{post.publishedAt}</time>
+<PortableText value={post.body} />
