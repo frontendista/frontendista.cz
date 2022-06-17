cfea5c04fae4bd66372ddd1092f63effcb795357
Pavel Sušický <susicky.pavel@outlook.cz>
2022-06-17 14:46:24 +0200

    refactor: remove trash from index page

--- apps/frontend/src/routes/index.svelte
+++ apps/frontend/src/routes/index.svelte
@@ -15,10 +15,3 @@
 <script lang="ts">
 	import Link from "$common/link.svelte";
 </script>
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<Link href="https://seznam.cz" />
-<Link href="/about" />
-<Link href="mailto:test@test.cz" />
