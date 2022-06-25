bf27513a4a0e1f9ab82ec727df7b3e035ae271c5
Pavel Sušický <susicky.pavel@outlook.cz>
2022-06-25 12:31:26 +0200

    feat(sidebar): add active state to sidebar navigation

--- apps/frontend/src/lib/components/side-bar-item.svelte
+++ apps/frontend/src/lib/components/side-bar-item.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
 	import { container, favicon, link, linkText } from "./side-bar-item.css";
 	import HiExternalLink from "$icons/hi-external-link.svg";
+	import HiChevronLeft from "$icons/hi-chevron-left.svg";
 
 	export let href: string;
 	export let icon: any = null;
+	export let isActive: boolean = false;
 
 	$: isExternal = href.startsWith("http");
 </script>
 
 <li class={container}>
-	<a class={link} {href}>
+	<a class={link} {href} aria-current={isActive ? "true" : null}>
 		{#if icon}
 			<svelte:component this={icon} height="1em" />
 		{:else if isExternal}
@@ -20,6 +22,8 @@
 		</span>
 		{#if isExternal}
 			<svelte:component this={HiExternalLink} height="1em" />
+		{:else if isActive}
+			<svelte:component this={HiChevronLeft} height="1em" />
 		{/if}
 	</a>
 </li>
