0c2bb65bddd3ed77a812525b9fac94fdebc13b4b
Pavel Sušický <susicky.pavel@outlook.cz>
2022-06-25 12:31:26 +0200

    feat(sidebar): add active state to sidebar navigation

--- apps/frontend/src/lib/components/side-bar-item.css.ts
+++ apps/frontend/src/lib/components/side-bar-item.css.ts
@@ -11,7 +11,22 @@ export const link = style({
 	width: "100%",
 	gap: "0.75rem",
 	paddingLeft: "0.75rem",
-	paddingRight: "0.75rem"
+	paddingRight: "0.75rem",
+	borderRadius: "0.75rem",
+	":hover": {
+		// TODO: Token, Transition
+		background: "#333"
+	},
+	":active": {
+		// TODO: Token
+		background: "#555"
+	},
+	selectors: {
+		"&[aria-current=true]": {
+			// TODO: Token
+			background: "#3A333A"
+		}
+	}
 });
 
 export const linkText = style({
