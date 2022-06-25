bd030545bcac84cb9f5648cf88a3765e2c5ea787
Pavel Sušický <susicky.pavel@outlook.cz>
2022-06-25 14:54:35 +0200

    feat(page): add basic page transition effect (#36)

---
+++ apps/frontend/src/lib/components/page-transition.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { fade } from "svelte/transition";
+
+	export let key: string;
+</script>
+
+{#key key}
+	<main
+		in:fade={{
+			delay: 500,
+			duration: 1000
+		}}
+		out:fade={{
+			duration: 500
+		}}
+	>
+		<slot />
+	</main>
+{/key}
