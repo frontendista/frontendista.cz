26f18f29fbed297a32ca49a7aa59391e1cd467da
Pavel Sušický <susicky.pavel@outlook.cz>
2022-06-25 09:21:37 +0200

    feat(sidebar): add sidebar item component

---
+++ apps/frontend/src/lib/components/side-bar-item.css.ts
@@ -0,0 +1,21 @@
+import { style } from "@vanilla-extract/css";
+
+export const container = style({
+	height: "2rem"
+});
+
+export const link = style({
+	display: "flex",
+	alignItems: "center",
+	height: "100%",
+	width: "100%",
+	gap: "0.75rem",
+	paddingLeft: "0.75rem",
+	paddingRight: "0.75rem"
+});
+
+export const linkText = style({
+	flexGrow: 1,
+	textTransform: "uppercase",
+	fontWeight: 800
+});
--- apps/frontend/src/lib/modules/footer.svelte
+++ apps/frontend/src/lib/modules/footer.svelte
@@ -1,11 +1,17 @@
+<script>
+	import SideBarItem from "$components/side-bar-item.svelte";
+
+	import { list } from "./side-bar.css";
+</script>
+
 <footer>
 	<section>
 		<h1>Runs on</h1>
-		<ul>
-			<li>Cloudflare</li>
-			<li>Sanity</li>
-			<li>Vercel</li>
-			<li>Wedos</li>
+		<ul class={list}>
+			<SideBarItem href="https://cloudflare.com">Cloudflare</SideBarItem>
+			<SideBarItem href="https://sanity.io">Sanity</SideBarItem>
+			<SideBarItem href="https://vercel.com">Vercel</SideBarItem>
+			<SideBarItem href="https://wedos.cz">Wedos</SideBarItem>
 		</ul>
 	</section>
 	<p>&copy; Pavel Sušický</p>
