d964fbc8ce3b685a9305ad844ffc2d4a12a3aee8
Pavel Sušický <susicky.pavel@protonmail.com>
2022-07-06 10:01:00 +0200

    chore(sveltekit): migrate to vite external config

---
+++ apps/frontend/vite.config.js
@@ -0,0 +1,43 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+import svg from "@poppanator/sveltekit-svg";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import { sveltekit } from "@sveltejs/kit/vite";
+
+export default defineConfig({
+	resolve: {
+		alias: {
+			$common: resolve("./src/lib/components/__common__"),
+			$components: resolve("./src/lib/components"),
+			$modules: resolve("./src/lib/modules"),
+			$stores: resolve("./src/lib/stores"),
+			$stylesheets: resolve("./src/lib/stylesheets"),
+			$utils: resolve("./src/lib/utils"),
+			$icons: resolve("./src/lib/icons")
+		}
+	},
+	server: {
+		port: 3000
+	},
+	plugins: [
+		sveltekit(),
+		svg({
+			includePaths: ["./src/lib/icons/"],
+			svgoOptions: {
+				multipass: true,
+				plugins: [
+					{
+						name: "preset-default",
+						params: {
+							overrides: {
+								removeViewBox: false
+							}
+						}
+					}
+				]
+			}
+		}),
+		vanillaExtractPlugin()
+	]
+});
