ee1fed8beea320c08abc715ec4fc47cfe1f0dedd
Pavel Sušický <susicky.pavel@protonmail.com>
2022-07-09 12:18:35 +0200

    feat(theming): add ThemeToggle icon enter transition

--- apps/frontend/src/lib/modules/top-bar.svelte
+++ apps/frontend/src/lib/modules/top-bar.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
 	import SideBarToggle from "$components/side-bar-toggle.svelte";
+	import ThemeToggle from "$components/theme-toggle.svelte";
 
 	import HiCog from "$icons/hi-cog.svg";
-	import HiSun from "$icons/hi-sun.svg";
-	import HiMoon from "$icons/hi-moon.svg";
-	import { themeStore } from "$stores/theme-store";
 
 	import { container, logo, actionBar, actionButton, actionBarButtonGroup } from "./top-bar.css";
 </script>
@@ -16,18 +14,7 @@
 	<div class={actionBar}>
 		<SideBarToggle />
 		<div class={actionBarButtonGroup}>
-			<button
-				aria-label={`Switch to ${$themeStore === "dark" ? "light" : "dark"} themes`}
-				class={actionButton}
-				on:click={themeStore.toggle}
-			>
-				{#if $themeStore === "dark"}
-					<svelte:component this={HiMoon} height="1em" />
-				{:else}
-					<svelte:component this={HiSun} height="1em" />
-				{/if}
-				<span>Theme</span>
-			</button>
+			<ThemeToggle />
 			<button class={actionButton}>
 				<svelte:component this={HiCog} height="1em" />
 				<span>Settings</span>
