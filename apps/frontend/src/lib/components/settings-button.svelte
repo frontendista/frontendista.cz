2ff07791b8a9bca9c651414346962e541ce562e7
Pavel Sušický <susicky.pavel@protonmail.com>
2022-07-10 11:30:26 +0200

    feat(settings): add popover panel

--- apps/frontend/src/lib/components/settings-button.svelte
+++ apps/frontend/src/lib/components/settings-button.svelte
@@ -1,27 +1,27 @@
 <script lang="ts">
+	import Switch from "$common/switch.svelte";
+	import { motionStore } from "$stores/motion-store";
 	import { actionButton } from "$modules/top-bar.css";
-
-	let rotation = 0;
-
-	function onClick() {
-		rotation += 360;
-	}
+	import { Popover, PopoverButton, PopoverPanel } from "@rgossiaux/svelte-headlessui";
+	import { settingsPanel } from "./settings-button.css";
 </script>
 
-<button class={actionButton} on:click={onClick}>
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		height="1em"
-		viewBox="0 0 20 20"
-		fill="currentColor"
-		style:transform={`rotate(${rotation}deg)`}
-		style:transition="transform 1s ease"
-	>
-		<path
-			fill-rule="evenodd"
-			d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
-			clip-rule="evenodd"
+<Popover>
+	<PopoverButton class={actionButton}>
+		<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 20 20" fill="currentColor">
+			<path
+				fill-rule="evenodd"
+				d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
+				clip-rule="evenodd"
+			/>
+		</svg>
+		<span>Settings</span>
+	</PopoverButton>
+	<PopoverPanel class={settingsPanel}>
+		<Switch
+			label="Reduce motion"
+			checked={$motionStore.value}
+			onChange={(e) => motionStore.setPreference(e.detail)}
 		/>
-	</svg>
-	<span>Settings</span>
-</button>
+	</PopoverPanel>
+</Popover>
