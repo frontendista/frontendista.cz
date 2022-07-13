5b685ab0f1b9c425c2c4e0999a401d739b0fa7d1
Pavel Sušický <susicky.pavel@protonmail.com>
2022-07-13 21:05:55 +0200

    fix(style): add style to SideBarToggle and a11y to buttons

--- apps/frontend/src/lib/components/side-bar-toggle.css.ts
+++ apps/frontend/src/lib/components/side-bar-toggle.css.ts
@@ -1,8 +1,11 @@
-import { actionButton } from "$modules/top-bar.css";
 import { style } from "@vanilla-extract/css";
 
+import { buttonStyle } from "$stylesheets/global.css";
+
 export const container = style([
-	actionButton,
+	buttonStyle({
+		type: "secondary"
+	}),
 	{
 		padding: 0,
 		width: "3rem",
--- apps/frontend/src/lib/modules/top-bar.css.ts
+++ apps/frontend/src/lib/modules/top-bar.css.ts
@@ -99,7 +99,16 @@ export const actionButton = style({
 globalStyle(`${actionButton} span`, {
 	"@media": {
 		"(max-width: 768px)": {
-			display: "none"
+			border: 0,
+			clip: "rect(0 0 0 0)",
+			clipPath: "inset(50%)",
+			height: 1,
+			margin: "0 -1px -1px 0",
+			overflow: "hidden",
+			padding: 0,
+			position: "absolute",
+			width: 1,
+			whiteSpace: "nowrap"
 		}
 	}
 });
