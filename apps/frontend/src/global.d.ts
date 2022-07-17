2dd3cd4dca9effd4679461c49f899e58ba2d1946
Pavel Sušický <susicky.pavel@protonmail.com>
2022-07-17 20:37:07 +0200

    refactor(ts): remove global.d.ts

--- apps/frontend/src/global.d.ts
+++
@@ -1,8 +0,0 @@
-import type { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers";
-
-declare global {
-	namespace Vi {
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		type Assertion<T = any> = TestingLibraryMatchers<T, void>;
-	}
-}
