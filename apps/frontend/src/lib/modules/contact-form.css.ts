ad5eb7ed3b514118cb7bd1f109b18a10fe66cdc8
Pavel Sušický <susicky.pavel@protonmail.com>
2022-07-17 13:21:44 +0200

    feat(form): add few style tweaks

--- apps/frontend/src/lib/modules/contact-form.css.ts
+++ apps/frontend/src/lib/modules/contact-form.css.ts
@@ -3,16 +3,21 @@ import { globalStyle, style } from "@vanilla-extract/css";
 export const container = style({
 	display: "flex",
 	flexFlow: "column wrap",
-	gap: "1rem",
-	padding: "1rem"
+	gap: "1rem"
 });
 
 export const formButtonGroup = style({
 	display: "flex",
 	gap: "1rem",
-	height: "3rem"
+	flexDirection: "column",
+	"@media": {
+		"(min-width: 768px)": {
+			flexDirection: "row"
+		}
+	}
 });
 
 globalStyle(`${formButtonGroup} > *`, {
-	flex: "1 1 auto"
+	flex: "1 1 auto",
+	textTransform: "uppercase"
 });
