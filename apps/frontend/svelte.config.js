70fa79cd3d2634517a0462399298690bf54941c2
Pavel Sušický <susicky.pavel@protonmail.com>
2022-07-17 19:07:49 +0200

    refactor(sveltekit): remove comments from config

--- apps/frontend/svelte.config.js
+++ apps/frontend/svelte.config.js
@@ -1,13 +1,14 @@
 import vercel from "@sveltejs/adapter-vercel";
 import preprocess from "svelte-preprocess";
 
-/** @type {import('@sveltejs/kit').Config} */
+/**
+ * @type {import('@sveltejs/kit').Config}
+ */
 const config = {
 	preprocess: preprocess({
 		postcss: true
 	}),
 	kit: {
-		// TODO: See usage https://github.com/sveltejs/kit/tree/master/packages/adapter-vercel#usage
 		adapter: vercel(),
 		prerender: {
 			default: true
