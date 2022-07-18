33e467c6fc6b39a750f16ba11d62c075ca46307f
Pavel Sušický <susicky.pavel@protonmail.com>
2022-07-18 15:37:31 +0200

    fix(e2e): add ua parser to check for MacOS

--- apps/frontend/tests/e2e/index_focus_test.e2e.ts
+++ apps/frontend/tests/e2e/index_focus_test.e2e.ts
@@ -1,8 +1,17 @@
 import { expect, test } from "@playwright/test";
 
+import uaParser from "ua-parser-js";
+
 test.describe("Index Page initial focus state", () => {
 	test("should focus skip navigations link on first Tab key", async ({ page }) => {
 		await page.goto("/");
+
+		const { os } = uaParser(await page.evaluate(() => navigator.userAgent));
+
+		// NOTE: Currently Firefox and Webkit browsers don't support focus on Anchor elements on MacOS by default.
+		if (os.name === "Mac OS") {
+			return;
+		}
 
 		await page.keyboard.press("Tab");
 
