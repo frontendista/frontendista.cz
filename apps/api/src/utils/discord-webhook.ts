065d35b839b088a2be53b7f8532b4a32b428e8a6
Pavel Sušický <susicky.pavel@protonmail.com>
2022-07-18 22:52:29 +0200

    feat(api): add message body content

--- apps/api/src/utils/discord-webhook.ts
+++ apps/api/src/utils/discord-webhook.ts
@@ -1,7 +1,29 @@
 import type { IContactBodySchema } from "./validation-schemes";
 
 export function createContactMessageBody({ message, name, email }: IContactBodySchema) {
+	const embed = {
+		timestamp: new Date(),
+		author: {
+			name
+		},
+		fields: [
+			{
+				name: "Message",
+				value: message,
+				inline: false
+			}
+		]
+	};
+
+	if (email) {
+		embed.fields.push({
+			name: "Email",
+			value: email,
+			inline: true
+		});
+	}
+
 	return {
-		content: "Test"
+		embeds: [embed]
 	};
 }
