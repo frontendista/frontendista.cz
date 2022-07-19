295a947bf51244753f0f157e3d10180ca7493695
Pavel Sušický <susicky.pavel@protonmail.com>
2022-07-19 21:56:17 +0200

    feat: migrate deps and schemas

--- apps/cms/src/schemas/author.ts
+++ apps/cms/src/schemas/author.ts
@@ -1,4 +1,6 @@
-export default {
+import { defineType } from "sanity";
+
+export default defineType({
 	name: "author",
 	title: "Author",
 	type: "document",
@@ -37,4 +39,4 @@ export default {
 			media: "avatar"
 		}
 	}
-};
+});
