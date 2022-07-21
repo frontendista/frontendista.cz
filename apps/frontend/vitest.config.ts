e252a80fa0cf6121a57e439681cfa0361e79c706
Pavel Sušický <susicky.pavel@protonmail.com>
2022-07-21 22:49:52 +0200

    test(unit): add sample test

--- apps/frontend/vitest.config.ts
+++ apps/frontend/vitest.config.ts
@@ -4,11 +4,11 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
-	plugins: [svelte({ hot: !process.env.VITEST })],
-	test: {
-		globals: true,
-		watch: false,
-		environment: "happy-dom",
-		setupFiles: "./tests/setup.ts"
-	}
+	plugins: [svelte({ hot: !process.env.VITEST })]
+	// test: {
+	// 	globals: true,
+	// 	watch: false,
+	// 	environment: "happy-dom",
+	// 	setupFiles: "./tests/setup.ts"
+	// }
 });
