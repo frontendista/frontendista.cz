99d5cbb4c523ad087017c5030212ae20bf1bbd80
Pavel Sušický <me@frontendista.cz>
2023-09-08 09:51:46 +0200

    feat(css): add testing brand utilities

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,10 +1,34 @@
+import plugin from "tailwindcss/plugin";
+
 import type { Config } from "tailwindcss";
 
 const config: Config = {
 	content: ["./src/**/*.astro"],
 	corePlugins: {
 		preflight: false
-	}
+	},
+	plugins: [
+		plugin(({ matchUtilities }) => {
+			matchUtilities({
+				brand: (value: unknown) => {
+					if (typeof value === "function") {
+						return {
+							color: value("brand")
+						};
+					}
+				
+					return {
+						color: value
+					};
+				}
+			}, {
+				values: {
+					"500": "oklch(65.35% 0.245 317.5 / <alpha-value>)"
+				},
+				type: "color"
+			});
+		})
+	]
 };
 
 export default config;
