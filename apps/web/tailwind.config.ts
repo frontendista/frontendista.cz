e74b9c0aab37bcfd992af2eb69ba48c3b1744d9e
Pavel Sušický <me@frontendista.cz>
2023-09-08 13:19:55 +0200

    feat(hero): add portrait photo

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -7,6 +7,14 @@ const config: Config = {
 	corePlugins: {
 		preflight: false
 	},
+	theme: {
+		padding: {
+			sm: "0.25rem",
+			md: "0.5rem",
+			lg: "1rem",
+			xl: "2rem"
+		}
+	},
 	plugins: [
 		plugin(({ matchUtilities }) => {
 			matchUtilities({
