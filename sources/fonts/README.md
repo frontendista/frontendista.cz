da05920b21c379e405ce8ba5c5b50b756a78d04d
Pavel Sušický <me@frontendista.cz>
2023-09-10 19:19:35 +0200

    feat(sources): add fort convertor

---
+++ sources/fonts/README.md
@@ -0,0 +1,20 @@
+# Fonts
+
+## 1. Usage
+
+### 1.1 Prerequisites
+
+- [**Python**](https://www.python.org/)
+- [**Fonttools**](https://github.com/fonttools/fonttools)
+
+> NOTE: For specific versions see the fonttools page on GitHub.
+
+### 1.2 Converting fonts
+
+Fonts are located in [files](./files) directory. Add more `*.ttf` files here to convert them. Converted files will be placed in `output` directory.
+
+```sh
+python convert.py
+
+# See ./output directory
+```
---
+++ sources/fonts/convert.py
@@ -0,0 +1,25 @@
+import os
+import glob
+
+from fontTools import subset
+
+current_directory = os.getcwd()
+
+ttf_files = glob.glob(os.path.join(current_directory, "files", '*.ttf'))
+
+os.makedirs("./output", exist_ok=True)
+
+for ttf_file in ttf_files:
+	file_name_with_extension = os.path.basename(ttf_file)
+	file_name_without_extension = os.path.splitext(file_name_with_extension)[0]
+
+	print(f"Converting: {file_name_without_extension}")
+
+	subset.main([
+		ttf_file,
+		"--unicodes-file=./unicodes.txt",
+		"--no-ignore-missing-unicodes",
+		"--layout-features=*",
+		f"--output-file=./output/{file_name_without_extension}.woff2",
+		"--flavor=woff2"
+	])
