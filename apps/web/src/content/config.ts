b19d0a126994c4052e0315f416ffec58d9164811
Pavel Sušický <me@frontendista.cz>
2023-09-20 13:51:06 +0200

    feat: add projects collection

---
+++ apps/web/src/content/config.ts
@@ -0,0 +1,20 @@
+import { z, defineCollection } from "astro:content";
+
+const projectCollection = defineCollection({
+	type: "data",
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		type: z.string(),
+		source: z.string().optional(),
+		year: z.number(),
+		cta: z.object({
+			text: z.string(),
+			href: z.string(),
+		}).optional()
+	})
+});
+
+export const collections = {
+	projects: projectCollection,
+};
