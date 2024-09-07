7ea47a88630ec2e240046e5dcd1adb323f53f6b2
Pavel Sušický <me@frontendista.cz>
2024-09-07 10:35:24 +0200

    feat(Textarea): add children content

--- apps/web/src/components/form/textarea.tsx
+++ apps/web/src/components/form/textarea.tsx
@@ -1,13 +1,16 @@
 import { clsx } from "clsx";
-import { forwardRef } from "preact/compat";
+import { forwardRef, useState } from "preact/compat";
 
 import type { JSX } from "preact";
 
 type TextareaProps = JSX.HTMLAttributes<HTMLTextAreaElement>;
 
 export const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(({ className, onInput, ...props }, ref) => {
+	const [content, setContent] = useState("");
 
 	const onInputInternal: JSX.InputEventHandler<HTMLTextAreaElement> = (event) => {
+		setContent(event.currentTarget.value);
+
 		if (onInput) {
 			onInput(event);
 		}
@@ -23,6 +26,8 @@ export const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(({ classN
 			class={clsx("min-h-[20rem] resize-none overflow-y-hidden leading-normal", className)}
 			onInput={onInputInternal}
 			{...props}
-		/>
+		>
+			{content}
+		</textarea>
 	);
 });
