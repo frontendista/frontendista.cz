b2bc823df3e9010312b8fd1eba9b3229e5e9e137
Pavel Sušický <me@frontendista.cz>
2024-09-07 17:00:42 +0200

    feat(mocks): edit GitHub sample data

--- apps/web/src/mocks/github.ts
+++ apps/web/src/mocks/github.ts
@@ -3,10 +3,10 @@ import { graphql } from "msw";
 const RELEASE_TAG_RESPONSE = {
 	repository: {
 		latestRelease: {
-			tagName: "frontend@2.0.0",
-			createdAt: "2023-01-06T20:33:09Z",
+			tagName: "frontend@0.0.0",
+			createdAt: "2000-01-01T20:33:09Z",
 			tagCommit: {
-				oid: "0f12ea28664829e5a43e6993de760cb4a24a4531"
+				oid: "00000008664829e5a43e6993de760cb4a24a4531"
 			},
 			url: "https://github.com/frontendista/frontendista.cz/releases/tag/frontend%401.7.0"
 		}
