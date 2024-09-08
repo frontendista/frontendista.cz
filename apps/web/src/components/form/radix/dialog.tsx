d9d7633d72c6a4201818266b9514139c1372c580
Pavel Sušický <me@frontendista.cz>
2024-09-08 16:56:49 +0200

    feat(dialog): add fade-in

--- apps/web/src/components/form/radix/dialog.tsx
+++ apps/web/src/components/form/radix/dialog.tsx
@@ -3,8 +3,8 @@ import * as Dialog from "@radix-ui/react-dialog";
 import { withClass } from "../hoc";
 
 export const Root = withClass(Dialog.Root, "");
-export const Overlay = withClass(Dialog.Overlay, "bg-primary/90 fixed inset-0");
-export const Content = withClass(Dialog.Content, "fixed inset-0 lg:top-[50%] lg:left-[50%] lg:max-w-[50rem] lg:-translate-x-1/2 lg:-translate-y-1/2 bg-secondary p-xl");
+export const Overlay = withClass(Dialog.Overlay, "bg-primary/90 fixed inset-0 animate-fade-in");
+export const Content = withClass(Dialog.Content, "fixed inset-0 lg:top-[50%] lg:left-[50%] lg:max-w-[50rem] lg:-translate-x-1/2 lg:-translate-y-1/2 bg-secondary p-xl animate-fade-in");
 export const Title = withClass(Dialog.Title, "");
 export const Description = withClass(Dialog.Description, "");
 export const Close = withClass(Dialog.Close, "");
