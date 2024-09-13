069bfabe3c4e85fbcdd56169ffc934dc063da97a
Pavel Sušický <me@frontendista.cz>
2024-09-13 18:36:53 +0200

    refactor

---
+++ packages/validation/src/utils.ts
@@ -0,0 +1,22 @@
+export function base64ToUint8Array(base64: string): Uint8Array {
+	const binary_string = atob(base64);
+	const length = binary_string.length;
+	const bytes = new Uint8Array(length);
+
+	for (let i = 0; i < length; i++) {
+		bytes[i] = binary_string.charCodeAt(i);
+	}
+
+	return bytes;
+}
+
+export function arrayBufferToBase64(buffer: ArrayBuffer): string {
+	let binary = '';
+
+	const bytes = new Uint8Array(buffer);
+
+	for (let i = 0; i < bytes.byteLength; i++) {
+		binary += String.fromCharCode(bytes[i]);
+	}
+	return btoa(binary);
+}
